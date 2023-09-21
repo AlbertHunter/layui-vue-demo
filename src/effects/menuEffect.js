67b4ce4b878257d5e505ec4be48d66c21c3e4748
tan <tan@gmail.com>
2023-09-21 18:02:22 +0800

    tab menu bug修改

---
+++ src/effects/menuEffect.js
@@ -0,0 +1,31 @@
+import {useRouter} from "vue-router/dist/vue-router";
+
+const useCommonMenuEffect = () => {
+    const router = useRouter()
+    //从路由获取菜单
+    const getMenus = () => {
+      const routeList = router.options.routes
+      const menuArr = []
+      let i = 0
+      for(const item of routeList) {
+        if(item?.show) {
+          i++
+          item.id = i
+          if(item.hasOwnProperty('children')) {
+            const children = item.children.filter((row) => row?.show)
+            item.children = children.map((v, index) => {
+              v.id = i * 100 + index
+              return v
+            })
+          }
+          menuArr.push(item)
+        } else {
+          continue
+        }
+      }
+      return menuArr
+    }
+    return { getMenus }
+  }
+  
+  export default useCommonMenuEffect
